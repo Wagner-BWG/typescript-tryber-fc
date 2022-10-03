76e9d9066f30c1a899a3c14913aa8cc506e2570a
Wagner B <wagner_mbarbosa@hotmail.com>
2022-10-02 21:04:07 -0300

    :exclamation:Implementa UserRoutes 	modified:   app/backend/src/routes/UserRoutes.ts

--- app/backend/src/routes/UserRoutes.ts
+++ app/backend/src/routes/UserRoutes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
+import LoginValidator from '../middlewares/loginValidator';
 import UserController from '../controllers/UserController';
-// import User from '../database/models/UserModel';
-// import UserService from '../services/UserService';
+import TokenGenerator from '../middlewares/tokenGenerator';
 
 const UserRoutes = Router();
 const userController = new UserController();
+const loginValidator = new LoginValidator();
+const tokenGenerator = new TokenGenerator();
 
-UserRoutes.post('/login', (req, res) => userController.findOne(req, res));
+UserRoutes.post(
+  '/login',
+  userController.findUser,
+  loginValidator.validateUser,
+  tokenGenerator.generateToken,
+);
 
 export default UserRoutes;
