cdb693499e635c11be642549e191ab3061eef13d
Wagner B <wagner_mbarbosa@hotmail.com>
2022-10-04 18:15:59 -0300

    :bangbang:FEAT: Implements the matches table Implements the model and migrations for matches table and seeds it.
    	new file:   app/backend/src/database/migrations/20221004205026-create-match.js
    	new file:   app/backend/src/database/models/MatchModel.ts
    	renamed:    app/backend/src/database/seeders/20211116145458-matches.js_ -> app/backend/src/database/seeders/20211116145458-matches.js

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,47 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  public id!: number;
+  public homeTeam: number;
+  public homeTeamGoals: number;
+  public awayTeam: number;
+  public awayTeamGoals: number;
+  public inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default Match;
