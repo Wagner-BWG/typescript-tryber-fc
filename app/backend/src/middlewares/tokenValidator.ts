d72ec52d0097d5cacb69512834c12a9ad218f941
Wagner B <wagner_mbarbosa@hotmail.com>
2022-10-05 23:37:17 -0300

    :heavy_exclamation_mark:FEAT: Implementes a tokenValidator middleware

---
+++ app/backend/src/middlewares/tokenValidator.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+
+class TokenValidator {
+  public validateToken =
+  async (req:Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    const token = req.headers.authorization;
+    const secret = process.env.JWT_SECRET || 'jwt_secret';
+
+    if (typeof token === 'string') {
+      try {
+        const user: string | jwt.JwtPayload | null = jwt.verify(token, secret);
+        if (user && typeof user === 'object') {
+          const { role } = user.data;
+          res.locals = role;
+          return next();
+        }
+      } catch (error) {
+        return res.status(400).json({ message: 'Invalid token' });
+      }
+    }
+    return res.status(400).json({ message: 'Invalid token' });
+  };
+}
+
+export default TokenValidator;
+//
